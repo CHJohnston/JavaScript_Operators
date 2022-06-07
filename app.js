54e9af61dd5ff26a1e702221427287a07fc34888
CHJohnston <cdh.johnston@gmail.com>
2022-06-07 14:23:54 -0500

    JS Operators - Completed Exercise

--- app.js
+++ app.js
@@ -5,15 +5,54 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
-
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+console.log("add: ", add);
+let minus = a - b;
+console.log("minus: ", minus);
+let multiply = a * b;
+console.log("multiply: ", multiply);
+let dividing = a / b;
+console.log("dividing: ", dividing);
+console.log("modulus: ", a % b);
+console.log("expontation: ", a ** b);
+console.log("Increment pre: ", ++a, a);
+console.log("Increment post: ", a++, a);
+console.log("Decrement pre: ", --a, a);
+console.log("Decrement post: ", a--, a);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
-
-// YOUR CODE HERE
-
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+console.log("num+str ", num + str);
+console.log("num+str2 ", num + str2);
+console.log("num+isPresent ", num + isPresent);
+console.log("firstName + num :", firstName + num);
+console.log("isPresent+str ", isPresent + str);
+console.log("firstName+lastName ", firstName + " " + lastName);
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+// Added _2 to variables that were used in Exercise 2 to avoid errors
 
-// YOUR CODE HERE
+console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str_2 = "5";
+let str2_2 = "five";
+let isPresent_2 = false;
+console.log("val == str_2 ", val == str_2);
+console.log("val === str_2 ", val === str_2);
+console.log("!isPresent_2 ", !isPresent_2);
+console.log(
+  "eleven” == str2 && val >= str ",
+  "eleven" == str2_2 && val >= str_2
+);
+console.log("!isPresent || isPresent ", !isPresent || isPresent);
+console.log("0==false ", 0 == false);
+console.log("0===false ", 0 === false);
+console.log("0!=false ", 0 != false);
+console.log("0!==false ", 0 !== false);
